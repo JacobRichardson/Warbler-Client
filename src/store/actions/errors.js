a612ab52fc9c59a807a10c2af035abf5efe6e2c6
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-27 14:47:40 -0400

    Creating src/store/actions/errors.js - Action Creators.
    This commit creates the action creators of addError and
    removeError for those action types.

---
+++ src/store/actions/errors.js
@@ -0,0 +1,34 @@
+/** 
+ * Error actions.
+ */
+
+// Imports.
+import { ADD_ERROR, REMOVE_ERROR } from '../actionTypes';
+
+/**
+ * Add error action creator.
+ * @export {Function} The addError function.
+ * @param {Object} error The error object.
+ * @returns {Object} The action to be dispatched.
+ */
+export function addError(error) {
+
+  // Return the action object with type and error.
+  return {
+    type: ADD_ERROR,
+    error
+  }
+}
+
+/**
+ * Remove error action creator.
+ * @export {Function} The removeError function.
+ * @returns {Object} The action to be dispatched.
+ */
+export function removeError() {
+
+  // Return the action object with type and error.
+  return {
+    type: REMOVE_ERROR
+  }
+}
