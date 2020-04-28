ecff38fb7418ec69f5341326f6f379fabbab20f2
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-28 13:39:48 -0400

    Updating src/containers/app.js - Authorization.
    This commit updates the app container to set the authorization
    token if one already exists in local storage. It will also
    log the user out of the token fails or has been manually tampered
    with. This also adds the ability to re-log the user in if
    something goes wrong with the app during runtime.

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,9 +9,30 @@ import { configStore } from '../store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar'
 import Main from './Main';
+import { setAuthorizationToken, setCurrentUser } from '../store/actions/auth';
+import jwtDecode from 'jwt-decode';
 
 //Invoke the config store method to get the initial store.
 const store = configStore();
+
+// If a JWT token already exists on local storage.
+if(localStorage.jwtToken) {
+
+  // Set the token.
+  setAuthorizationToken(localStorage.jwtToken);
+
+  // Prevent someone from manually tampering with jwtToken in local storage.
+  try {
+
+    // Set the current user to the result of decoding the token (the payload).
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+
+  } catch (e) {
+
+    // Because there is an error, log the user out.
+    store.dispatch(setCurrentUser({}));
+  }
+}
 
 /** 
  * Main app function.
