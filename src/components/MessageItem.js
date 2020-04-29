286e939ba724201da9be619e5fca2f1d6ebdcc79
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-28 21:38:26 -0400

    Create src/components/MessageItem.js
    This commit creates the message item component. This component
    displays the information related to a single message.

---
+++ src/components/MessageItem.js
@@ -0,0 +1,45 @@
+/** 
+ * MessageItem Component.
+*/
+
+// Imports.
+import React from 'react';
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
+import DefaultProfileImg from '../images/default-profile-image.jpg';
+
+/**
+ * MessageItem Component
+ * @param {Object} props The props for the component.
+ * @returns JSX for the component.
+ */
+function MessageItem(props) {
+
+    // Retrieve values from props.
+    const { date, profileImageUrl, text, username } = props;
+
+    // Return JSX.
+    return (
+        <div>
+            <img 
+                src={profileImageUrl || DefaultProfileImg} 
+                alt={username} 
+                height="100"
+                width="100" 
+                className="timeline-image"
+            />
+            <div className="message-area">
+                <Link to="/">@{username} &nbsp;</Link>
+                <span className="text-muted">
+                    <Moment className="text-muted" format="DD MM YYYY">
+                        {date}
+                    </Moment>
+                </span>
+                <p>{text}</p>
+            </div>
+        </div>
+    )
+}
+
+// Export the component.
+export default MessageItem;
