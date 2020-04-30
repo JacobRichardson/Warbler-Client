60ff2c336dd53fbcd639c2c1b90df5689fdfb657
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-29 20:15:29 -0400

    Updating src/containers/MessageList.js - Styling.
    This commit updates the message list to style the message list
    to look better.

--- src/containers/MessageList.js
+++ src/containers/MessageList.js
@@ -53,8 +53,16 @@ class MessageList extends Component {
             />
         ));
 
-        // Return the message list.
-        return messageList;
+        // Return JSX.
+        return (
+            <div className="row col-sm-8">
+                <div className="offset-1 col-sm-10">
+                    <ul className="list-group" id="messages">
+                        {messageList}
+                    </ul>
+                </div>
+            </div>
+        );
     }
 }
 
