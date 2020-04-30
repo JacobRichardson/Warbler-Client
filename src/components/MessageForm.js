fa1e8043120dcad81b4a4c3e96a3fbc3515a328d
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-29 20:45:36 -0400

    Creates src/components/MessageForm.js.
    This commit creates the skeleton for the MessageForm component.
    The actual functionality and look of the form still needs to be
    created.

---
+++ src/components/MessageForm.js
@@ -0,0 +1,26 @@
+/** 
+ * MessageForm Component.
+*/
+
+// Imports.
+import React from 'react';
+
+/**
+ * MessageForm Component.
+ * @param {Object} props The props for the component.
+ * @returns JSX to be rendered.
+ */
+function MessageForm(props) {
+
+    // TODO: Implement MessageForm.
+
+    // Return JSX.
+    return (
+        <div>
+            Message Form
+        </div>
+    );
+}
+
+// Export the MessageForm Component.
+export default MessageForm;
