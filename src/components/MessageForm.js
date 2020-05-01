be76c0ff67a899ddff76d067d7922a4b47faf9c1
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-30 22:29:08 -0400

    Updates src/components/MessageForm - Creating new message.
    This commit builds out the create new message message form and
    uses the new postNewMessage action to actually create the message.
    The form is working properly and is creating new messages.

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -3,24 +3,99 @@
 */
 
 // Imports.
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { postNewMessage } from '../store/actions/messages'
 
 /**
- * MessageForm Component.
- * @param {Object} props The props for the component.
- * @returns JSX to be rendered.
+ * Message Form Component.
+ * @class MessageForm
+ * @extends {Component}
  */
-function MessageForm(props) {
+class MessageForm extends Component {
 
-    // TODO: Implement MessageForm.
+    /**
+     * Creates an instance of MessageForm.
+     * @param {Object} props The props for the component.
+     * @memberof MessageForm
+     */
+    constructor(props) {
 
-    // Return JSX.
-    return (
-        <div>
-            Message Form
-        </div>
-    );
+        // Invoke parent's constructor.
+        super(props);
+
+        // Set default state.
+        this.state = {
+            message: ""
+        };
+    }
+
+    /**
+     * Handles creating a new message.
+     * @param {Event} event The form submission event.
+     * @memberof MessageForm
+     */
+    handleNewMessage = event => {
+
+        // Prevent the default action.
+        event.preventDefault();
+
+        // Use the postNewMessage action with what the user typed in
+        // in order to create the message.
+        this.props.postNewMessage(this.state.message);
+
+        // Clear the message.
+        this.setState({message: ""});
+
+        // Move the user back to their timeline.
+        this.props.history.push('/');
+
+    }
+
+    // Render method to render the component..
+    render() {
+
+        // Return JSX.
+        return (
+            <div className="message-form-container">
+                <h2>Post a new message on your timeline!</h2>
+                <form className="message-form mt-5" onSubmit={this.handleNewMessage}>
+
+                    {/* If there are any errors. */}
+                    {this.props.errors.message && (
+                        <div className="alert alert-danger">
+                            {this.props.errors.message}
+                        </div>
+                    )}
+
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={this.state.message}
+                        onChange={e => this.setState({ message: e.target.value})}
+                    />
+
+                    <button className="btn btn-success mt-3 float-right" type="submit">
+                        Add my message
+                    </button>
+
+                </form>
+            </div>
+        );
+    }
 }
 
-// Export the MessageForm Component.
-export default MessageForm;
+/**
+ * Maps redux state to props for the component.
+ * @param {Object} state The Redux state.
+ * @returns {Object} The props for the component.
+ */
+function mapStateToProps(state) {
+    return {
+        errors: state.errors
+    }
+}
+
+// Export connecting the redux state to the component
+// by mapping state to props and passing in dispatch events.
+export default connect(mapStateToProps, { postNewMessage })(MessageForm);
