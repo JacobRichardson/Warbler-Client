e5160cdb385c2a01d514d9ff692afd96f41d1da6
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-05-07 16:04:04 -0400

    Updating src/store/reducers/index.js - User reducer.
    This commit updates the index reducer to include the new user
    reducer.

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -7,12 +7,14 @@ import { combineReducers } from "redux";
 import currentUser from './currentUser';
 import errors from "./errors";
 import messages from './messages';
+import user from './user';
 
 //Use combine reducers to combine all the reducers.
 const rootReducer = combineReducers({
   currentUser,
   errors,
-  messages
+  messages,
+  user
 });
 
 //Export the root reducer.
