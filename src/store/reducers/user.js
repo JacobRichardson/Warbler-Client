039db7051f2a5e201f2ac3cded474520994551df
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-05-07 16:01:17 -0400

    Creates src/store/reducers/user.js.
    This commit creates the user reducer. This reducer currently only
    has the ability to load a user. This means a user can be fetched
    and them be put onto the redux store.

---
+++ src/store/reducers/user.js
@@ -0,0 +1,25 @@
+/** 
+ * User Reducer.
+ */
+
+// Imports
+import { LOAD_USER } from '../action-types';
+
+/**
+ * Handles modifying state in regards to users.
+ * @param {Array} [state=[]] The state.
+ * @param {Object} action The action object.
+ */
+function user(state = [], action) {
+
+  // Switch on the action type.
+  switch (action.type) {
+    case LOAD_USER:
+      return action.user;
+    default:
+      return state;
+  }
+}
+
+// Export the user reducer.
+export default user;
